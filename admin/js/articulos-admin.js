508d52a93148c6ed2d1f246034a77ef23948c9d1
fpacheco53 <fr.pachecos@duocuc.cl>
2026-09-07 03:00:05 -0300

    Admin, datos dinamicos, control de stock, inicio añadir producto
    Se modificó la pagina web de admin/dashboard para que esta contenga datos dinamicos que se rellenan via javascript, header se mantiene como dato fijo, se añadio un nuevo color en el style.css junto a otras clases para el panel de admin, los botones de manejo de stock tienen una pequeña validacion para evitar stocks negativos, y articulos admin usa un for para generar los articulos, ahora se va a trabajar en el dashboard/productos y que tenga un formulario para añadir articulos

---
+++ admin/js/articulos-admin.js
@@ -0,0 +1,33 @@
+const catalogo = document.getElementById("catalogo");
+
+for (let i = 1; i <= 5; i++) {
+  const articulo = document.createElement("article");
+  articulo.className = "caluga";
+
+  articulo.innerHTML = `
+    <div class="contenedor-img">
+        <img
+            src="https://placehold.net/400x400.png"
+            alt="Articulo ${i}"
+            class="imagen_aju"
+        />
+    </div>
+
+    <div class="info-producto">
+        <h3 class="tituloJuego">Articulo ${i}</h3>
+        <span class="precioJuego">$64.990</span>
+
+        <div class="stock-producto">
+            <button class="btn-stock btn-menos">-</button>
+            <p id="producto-${i}" class="id-producto">${i}</p>
+            <button class="btn-stock btn-mas">+</button>
+        </div>
+
+        <button class="btn-guardar" onclick="console.log('Se guardo el stock del articulo con la ID:', ${i})">
+            GUARDAR
+        </button>
+    </div>
+`;
+
+  catalogo.appendChild(articulo);
+}
